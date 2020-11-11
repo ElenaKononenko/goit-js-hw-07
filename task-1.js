70e8bdcbc0eb8f8e938210a2ef8174096e4d526a
ElenaKononenko <morra4444@gmail.com>
2020-11-11 13:43:48 +0200

    task1

---
+++ task-1.js
@@ -0,0 +1,15 @@
+
+
+const listItem = document.querySelectorAll(".item")
+console.log(`В списке ${listItem.length} категории`)
+
+
+
+listItem.forEach((el) => {el.firstElementChild.textContent
+
+  let categorie = el.firstElementChild.textContent
+ let  total = el.querySelectorAll("li").length
+  console.log(`   Категория: ${categorie}
+   Количество элементов: ${total}`)
+}
+)
